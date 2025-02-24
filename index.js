d366a04c7a830bef577ce3c3f30de74ea641fd5c
theMungai <mungaithesimon@gmail.com>
2025-02-24 23:24:58 +0300

    JavaScript Functions

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Follow along with the examples here
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+}
+  
+sayHelloTo("Guadalupe");
+sayHelloTo("Jane");
+sayHelloTo("R2-D2");
+sayHelloTo(1); 
+
+function say(greeting, firstName) {
+console.log("firstName: ", firstName);
+console.log("greeting: ", greeting);
+console.log(`${greeting}, ${firstName}!`);
+}
+
+say("Julio", "hello");
+
+function add(x, y) {
+    return x + y;
+}
+console.log(add(1, 2));
